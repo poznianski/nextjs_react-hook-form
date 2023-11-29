4152623ae5f31e05a2db6dd33a3abb53a2647043
Viacheslav Miasoiedov <poznianski@gmail.com>
2023-11-29 15:39:00 +0200

    rdy to deploy

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -14,6 +14,7 @@ export const Header: React.FC = () => {
         sm:flex-row sm:justify-between sm:px-6 lg:px-8"
     >
       <Logo />
+
       <SearchInput onSearchChange={handleSearchChange} />
     </header>
   )
--- src/app/_components/Logo/Logo.tsx
+++ src/app/_components/Logo/Logo.tsx
@@ -1,16 +1,38 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Logo: React.FC = () => {
+  const [isImageVisible, setIsImageVisible] = useState(false)
+
+  const toggleImage = () => {
+    setIsImageVisible(!isImageVisible)
+  }
+
   return (
-    <div className="flex gap-2.5 justify-items-center items-center">
+    <div
+      className="flex cursor-pointer items-center justify-items-center gap-2.5"
+      onClick={toggleImage}
+    >
       <Image
         src="/images/logo.svg"
         alt="logo"
         width={78}
         height={30}
+        className=""
       />
-      <span className="text-32 text-white font-bold">Memes</span>
+
+      <span className="text-32 font-bold text-white">Memes</span>
+
+      {isImageVisible && (
+        <div className="meme">
+          <Image
+            src="/images/meme.webp"
+            alt="logo"
+            width={78}
+            height={30}
+          />
+        </div>
+      )}
     </div>
   )
 }
